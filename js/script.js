f35db6704985303864d57073be46d6e856fc6e7a
Davide <ciampad06@gmail.com>
2021-01-03 10:28:56 +0100

    Fine parziale della milestone 2 e fine della 3

--- js/script.js
+++ js/script.js
@@ -3,41 +3,64 @@ var app = new Vue(
     el:"#root",
     data:{
       searchString:"",
-      poster:"",
-      filmVote: 0,
+      poster:"https://image.tmdb.org/t/p/w220_and_h330_face",
       films:[],
+      flags:[
+        "https://hotelbolognasrl.it/wp-content/uploads/2016/06/uk.png",
+        "https://i.pinimg.com/originals/f8/9c/9c/f89c9c46646b38537e45f113f829855e.png",
+      ],
+      // poster:"https://image.tmdb.org/t/p/" + getFlag(film)
     },
     methods:{
-      searchMovie: function(){
-
+      search: function(){
         const self = this;
-        axios.get("https://api.themoviedb.org/3/search/movie",{
-            params:{  // per ora qui solo API key e query di ricerca
-              api_key: "8254ee6bdff5c0d69e1229a04775b2cb",
-              query: self.searchString,
+        Promise.all([
+          axios.get("https://api.themoviedb.org/3/search/tv",{
+              params:{  // per ora qui solo API key e query di ricerca
+                api_key: "8254ee6bdff5c0d69e1229a04775b2cb",
+                query: self.searchString,
+              }
             }
-          }
-        )
-        .then(function(result){
-          console.log(result.data.results);
-          self.films = result.data.results;
-          // self.poster = result.data.result.poster_path;
+          ),
+          axios.get("https://api.themoviedb.org/3/search/movie",{
+              params:{  // per ora qui solo API key e query di ricerca
+                api_key: "8254ee6bdff5c0d69e1229a04775b2cb",
+                query: self.searchString,
+              }
+            }
+          )
+          .then(function(result){
+            console.log(result.data.results);
+            self.films = result.data.results
+            // - ricerca anche serie tv
+          })
+        ])
+      },
+      getVote(film) {
+        // - generazione stelline -> result.data.results[0].vote_average
+        // arrotondo il voto per eccesso usando il math ceil
+        return parseInt(film.vote_average / 2);
+      },
 
-          // - generazione stelline -> result.data.results[0].vote_average
+      // getFlag(film) {
 
-          for(let i = 0; i<result.data.results.length; i++){
-            let voto = Math.ceil(Math.ceil(result.data.results[i].vote_average) / 2);
-            self.filmVote = voto;
-            console.log(voto);
-          };
-
-
-
-          // - gestione lingue -> result.data.results[0].original_language
-
-          // - ricerca anche serie tv
-      })
-      }
+        // - gestione lingue -> result.data.results[0].original_language
+        // film.original_language
+        //
+        // if(film.original_language == "en" ){
+        //   return this.flags[0];
+        // }else if (film.original_language == "it") {
+        //   return this.flags[1];
+        // }else {
+        //   return film.original_language;
+        // };
+      //
+      // getImage(film){
+      //   return film.poster_path;
+      // }
+      //
+      //
+      // }
     },
   })
 
